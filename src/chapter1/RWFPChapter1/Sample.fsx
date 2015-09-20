30413b7ad9aefd2ea8b1c25aa5b36e4be2ae9b87
unknown <ud1815v@USGSOW10228678.vcn.ds.volvo.net>
2015-09-19 20:11:09 -0400

    Chapter 1

---
+++ src/chapter1/RWFPChapter1/Sample.fsx
@@ -0,0 +1,12 @@
+﻿let message = "Hello World!"
+printfn "%s" message
+
+let name = "Tomas"
+let str = "Hello " + name + "!"
+
+let sayHello(name) =
+    let str = "Hello " + name + "!"
+    printfn "%s" str
+
+
+
